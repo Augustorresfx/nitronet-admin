35064871ef5b5f1a06987337a18400bb75671a2c
Augustorresfx <augustorresfx@gmail.com>
2022-10-18 21:48:21 -0300

    testing-deploy-7

--- app/app.py
+++ app/app.py
@@ -199,7 +199,7 @@ if __name__=='__main__':
     create_app = create_app()
     create_app.run()
 else:
-    gunicorn_app = create_app()
+    gunicorn_app = app.run()
     """app.config["MYSQL_USER"] = os.getenv('MYSQL_USER')
     app.config["MYSQL_PASSWORD"] = os.getenv('MYSQL_PASSWORD')
     app.config["MYSQL_DB"] = os.getenv('MYSQL_DB')
